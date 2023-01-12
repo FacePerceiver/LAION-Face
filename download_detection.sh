3c78a23c9d7bdba985c96f94d512712b600339d8
Yiming Lin <yiminglin@meta.com>
2023-01-12 12:53:48 +0000

    retry download detection

--- download_detection.sh
+++ download_detection.sh
@@ -1,8 +1,16 @@
+#! /bin/bash
 folder="$1"
 mkdir $folder
 for i in $(seq 0 31)
 do
     echo "start download face detection $i"
-    wget https://huggingface.co/datasets/FacePerceiver/laion-face/resolve/main/sample2detect_$i.pth -O $folder/sample2detect_$i.pth
+    while true 
+    do
+    if [ -s $folder/sample2detect_$i.pth ]; then
+            break
+    else
+      wget --no-proxy https://huggingface.co/datasets/FacePerceiver/laion-face/resolve/main/sample2detect_$i.pth -O $folder/sample2detect_$i.pth
+    fi
+    done
     echo "end download part $i"
 done
